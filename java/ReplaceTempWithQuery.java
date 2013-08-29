f0948a3789d1427f61efcf5c09bd0914057361f6
SatoShun <shun.sato1@gmail.com>
2013-08-29 20:02:54 +0900

    add method: calculate holiday distance

--- java/ReplaceTempWithQuery.java
+++ java/ReplaceTempWithQuery.java
@@ -8,7 +8,7 @@ class Walk {
         this.times = times;
     }
 
-    public double weekDayDistance() {
+    public double weekdayDistance() {
         double distance = speedPerHour * times;
 
         if (distance >= 10.0) {
@@ -16,6 +16,16 @@ class Walk {
         }
 
         return distance * 5.0;
+    }
+
+    public double holidayDistance() {
+        double distance = speedPerHour * times;
+
+        if (distance >= 10.0) {
+            distance *= 0.9;
+        }
+
+        return distance * 2.0;
     }
 
     public String toString() {
@@ -33,8 +43,12 @@ class Walk2 {
         this.times = times;
     }
 
-    public double weekDayDistance() {
+    public double weekdayDistance() {
         return baseDistance() * distanceCompensation() * 5.0;
+    }
+
+    public double holidayDistance() {
+        return baseDistance() * distanceCompensation() * 2.0;
     }
 
     private double baseDistance() {
@@ -56,10 +70,12 @@ class ReplaceTempWithQuery {
     public static void main(String[] args) {
         Walk w = new Walk(50.5, 3.5);
         System.out.println(w);
-        System.out.println(w.weekDayDistance());
+        System.out.println(w.weekdayDistance());
+        System.out.println(w.holidayDistance());
 
         Walk2 w2 = new Walk2(50.5, 3.5);
         System.out.println(w2);
-        System.out.println(w2.weekDayDistance());
+        System.out.println(w2.weekdayDistance());
+        System.out.println(w2.holidayDistance());
     }
 }
